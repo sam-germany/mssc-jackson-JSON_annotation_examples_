eeef864d98c2683fd109f9cbf37d75490340bd98
sunny <sunny.keila24@gmail.com>
2020-11-26 19:09:27 +0100

    service created

---
+++ src/test/java/com/springframework/jackson/MsscJacksonExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.jackson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscJacksonExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/springframework/jackson/model/BeerDtoTest.java
@@ -0,0 +1,51 @@
+package com.springframework.jackson.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+
+import java.io.IOException;
+
+
+
+@JsonTest
+class BeerDtoTest extends BaseTest{
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test          // hier serialization means that their is a object and we are converting it to a JSON object
+    void testSerializeDto22() throws JsonProcessingException {
+        BeerDto beerDto = getDto();
+
+        String jsonString22 = objectMapper.writeValueAsString(beerDto);
+
+        System.out.println(jsonString22);
+    }
+
+    @Test
+    void testDeserialize22() throws IOException {
+        String json22 = "{\"id\":\"639c00ff-9993-4ec1-af12-8277c85f93bb\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":123123123123,\"price\":12.99,\"createdDate\":\"2019-06-02T16:35:58.321001-04:00\",\"lastUpdatedDate\":\"2019-06-02T16:35:58.321872-04:00\"}";
+        System.out.println("AAA : " + json22);
+
+
+        BeerDto dto = objectMapper.readValue(json22, BeerDto.class);
+        System.out.println(dto);
+
+/* json22  = {
+               "id" : "639c00ff-9993-4ec1-af12-8277c85f93bb",
+               "beerName" : "BeerName",
+               "beerStyle" : "Ale",
+               "upc" : 123123123123,
+               "price" : 12.99,
+               "createdDate" : "2019-06-02T16:35:58.321001-04:00",
+               "lastUpdatedDate" : "2019-06-02T16:35:58.321872-04:00"
+             }
+
+he created a JSON object  \"   it will print  "   only one doublequote
+just for showing purpose he create this json object.
+ */
+    }
+}
